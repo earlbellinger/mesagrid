5fad3bec4110071be301afe91ace47570f3295de
Earl Bellinger <earlbellinger@gmail.com>
2023-10-18 16:59:51 +0200

    fixed descriptors

--- setup.py
+++ setup.py
@@ -19,14 +19,16 @@ setup(
     author_email='earl.bellinger@yale.edu',
     python_requires='>=3.6',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
+        'Development Status :: 6 - Mature',
+        'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Scientific/Engineering :: Physics'
     ],
     description="Parse grids of MESA tracks and models",
     install_requires=requirements,
