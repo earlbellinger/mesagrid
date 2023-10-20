f1cb13861e03d48e4e31c20decc593298ebae279
Earl Bellinger <earlbellinger@gmail.com>
2023-10-20 11:47:02 +0200

    fix version

--- mesagrid/__init__.py
+++ mesagrid/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for mesagrid."""
 
 from importlib.metadata import version
-__version__ = version("pycounts")
+__version__ = version("mesagrid")
 
 __author__ = """Earl Patrick Bellinger"""
 __email__ = 'earl.bellinger@yale.edu'
