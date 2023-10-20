692f17185eb010d4bfc4b9dcc9c75cc9a074f57b
Earl Bellinger <earlbellinger@gmail.com>
2023-10-20 13:05:30 +0200

    imports

--- mesagrid/__init__.py
+++ mesagrid/__init__.py
@@ -6,4 +6,4 @@ __version__ = version("mesagrid")
 __author__ = """Earl Patrick Bellinger"""
 __email__ = 'earl.bellinger@yale.edu'
 
-from mesagrid.star import *
+from mesagrid.star import Track, Grid, load_history_seismology
