4bcf735041ff5a3b7cb42f69511961108e33bb27
Earl Bellinger <earlbellinger@gmail.com>
2023-10-20 11:44:53 +0200

    init

--- mesagrid/__init__.py
+++ mesagrid/__init__.py
@@ -1,5 +1,9 @@
 """Top-level package for mesagrid."""
 
+from importlib.metadata import version
+__version__ = version("pycounts")
+
 __author__ = """Earl Patrick Bellinger"""
 __email__ = 'earl.bellinger@yale.edu'
-__version__ = '0.1.0'
+
+from mesagrid import Grid, Track
