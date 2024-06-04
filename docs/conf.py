65e61aacdf1be769f5d55088795d5c4502b035b0
Earl Patrick Bellinger <epb37@voltron.stdusr.yale.internal>
2024-06-03 23:56:10 -0400

    sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ import mesagrid
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'myst_parser', 'myst_nb']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'myst_nb']#, 'sphinxawesome_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -64,7 +64,7 @@ release = mesagrid.__version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -83,7 +83,9 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinxawesome-theme'
+#html_permalinks_icon = '<span>#</span>'
+#html_theme = 'sphinxawesome-theme'
+html_theme = 'sphinx-rtd-theme'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
