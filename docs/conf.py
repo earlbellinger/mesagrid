9976402bdd47cbbc77f05b6a5ff71d38bbcdb770
earlbellinger <earlbellinger@gmail.com>
2024-06-11 12:09:41 -0400

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -108,6 +108,7 @@ html_theme = 'sphinxawesome_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = ["custom.css"]
 
 html_favicon = 'favicon.png'
 
