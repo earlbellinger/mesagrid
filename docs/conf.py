7d3fc09bba9d0698f2b0c61e42365a0f2be32892
earlbellinger <earlbellinger@gmail.com>
2024-06-12 20:41:14 -0400

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,16 @@ import mesagrid
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'myst_nb']#, 'sphinxawesome_theme']
+#extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'myst_nb']#, 'sphinxawesome_theme']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.viewcode",
+    "sphinx_sitemap",
+    "sphinx_design",
+    "myst_nb"
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -107,8 +116,8 @@ html_theme = 'sphinxawesome_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-html_css_files = ["custom.css"]
+#html_static_path = ['_static']
+#html_css_files = ["custom.css"]
 
 html_favicon = 'favicon.png'
 
